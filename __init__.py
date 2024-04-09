85f3bab11df7594b72a35f540ad60e803f28bc0f
chinhtranvn <45981707+chinhtranvn@users.noreply.github.com>
2024-04-09 09:30:58 +0700

    Update __init__.py
    fix lỗi khai báo

--- __init__.py
+++ __init__.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
-from odoo import api, SUPERUSER_ID, _
-from odoo.addons.base.models.res_model import get_models
+from odoo import api, SUPERUSER_ID
 
 def post_init_hook(cr, registry):
-  """
-  Hàm này được thực hiện sau khi module được cài đặt.
-  Sửa trạng thái của các đơn hàng có trạng thái trống thành 'consignment'.
-  """
-  env = api.Environment(cr, SUPERUSER_ID, {})
-  model_names = get_models(env, ['sale.order'])
-  if 'sale.order' in model_names:
-    sale_order_obj = env['sale.order']
-    consignment_orders = sale_order_obj.search([('state', '=', False)])
-    if consignment_orders:
-      consignment_orders.write({'state': 'consignment'})
-      # Ghi log thông báo
-      env['ir.logging'].create({
-        'name': 'Consignment module',
-        'level': 'info',
-        'message': _('Đã cập nhật trạng thái cho %d đơn hàng thành "ký gửi".') % len(consignment_orders),
-      })
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        sale_order_obj = env['sale.order']
+        consignment_orders = sale_order_obj.search([('state', '=', False)])
+        if consignment_orders:
+            consignment_orders.write({'state': 'consignment'})
