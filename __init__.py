417122be9309220da6d6c2ac0d43811ec4200ba0
chinhtranvn <45981707+chinhtranvn@users.noreply.github.com>
2024-04-09 11:36:10 +0700

    Update __init__.py
    add form. import models

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import api, SUPERUSER_ID
+from . import models
 
 def post_init_hook(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        sale_order_obj = env['sale.order']
-        consignment_orders = sale_order_obj.search([('state', '=', False)])
-        if consignment_orders:
-            consignment_orders.write({'state': 'consignment'})
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    sale_order_obj = env['sale.order']
+    consignment_orders = sale_order_obj.search([('state', '=', False)])
+    if consignment_orders:
+        consignment_orders.write({'state': 'consignment'})
